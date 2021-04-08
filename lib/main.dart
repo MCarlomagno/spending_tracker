89be7277cb3b751d7df84ad6ce54e21a18c5bb52
mcarlomagno <marcoscarlomagno1@gmail.com>
2021-04-07 21:21:33 -0300

    feat: heading ui

--- lib/main.dart
+++ lib/main.dart
@@ -10,15 +10,13 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Spending tracker',
       theme: ThemeData(primarySwatch: Colors.blue, fontFamily: 'Poppins'),
-      home: MyHomePage(title: 'Spending tracker'),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, this.title}) : super(key: key);
-
-  final String? title;
+  MyHomePage({Key? key}) : super(key: key);
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
@@ -27,64 +25,41 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
+    const headerHeight = 200.0;
+    const headerTextWidth = 400.0;
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          widget.title!,
-          style: TextStyle(fontFamily: 'Poppins'),
-        ),
-      ),
-      body: SizedBox.expand(
-        child: GestureDetector(
-            child: Container(
-              color: Colors.black,
-            ),
-            onVerticalDragUpdate: (details) {
-              if (details.delta.dy < 10) {
-                Navigator.of(context).push(_createRoute());
-              }
-            }),
-      ),
-    );
-  }
-
-  Route _createRoute() {
-    return PageRouteBuilder(
-      pageBuilder: (context, animation, secondaryAnimation) => SecondRoute(),
-      transitionsBuilder: (context, animation, secondaryAnimation, child) {
-        var begin = Offset(0.0, 1.0);
-        var end = Offset.zero;
-        var curve = Curves.ease;
-
-        var tween =
-            Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
-
-        return SlideTransition(
-          position: animation.drive(tween),
-          child: child,
-        );
-      },
-    );
-  }
-}
-
-class SecondRoute extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Add"),
-      ),
-      body: GestureDetector(
-          child: Container(
-            color: Colors.pink,
-          ),
-          onVerticalDragUpdate: (details) {
-            if (details.delta.dy > 10) {
-              print("swipe to bottom");
-              Navigator.pop(context);
-            }
-          }),
-    );
+        appBar: AppBar(
+            backgroundColor: Colors.transparent,
+            elevation: 0,
+            toolbarHeight: headerHeight,
+            actions: [
+              Padding(
+                padding: EdgeInsets.all(20),
+                child: Align(
+                  alignment: Alignment.topRight,
+                  child: IconButton(
+                    onPressed: () {},
+                    icon: Icon(
+                      Icons.more_vert,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+              )
+            ],
+            title: Container(
+              padding: EdgeInsets.fromLTRB(20, 20, 0, 0),
+              width: headerTextWidth,
+              height: headerHeight,
+              child: Text(
+                "Welcome back,\nMarcos",
+                style: TextStyle(
+                  fontFamily: 'Poppins',
+                  fontSize: 30,
+                  color: Colors.black,
+                ),
+              ),
+            )),
+        body: SizedBox.expand());
   }
 }
