024733e305442e77341fa45ea970371c9e2f3b97
mcarlomagno <marcoscarlomagno1@gmail.com>
2021-04-11 14:50:20 -0300

    feature: show loader after saving transaction

--- lib/widgets/home/add_transaction_form.dart
+++ lib/widgets/home/add_transaction_form.dart
@@ -5,6 +5,7 @@ import 'package:spending_tracker/models/transactions_model.dart';
 import 'package:spending_tracker/services/transactions_service.dart';
 import 'package:spending_tracker/setup.dart';
 import 'package:spending_tracker/widgets/shared/app_text_field.dart';
+import 'package:spending_tracker/widgets/shared/button_progress.dart';
 import 'package:spending_tracker/widgets/shared/subtitle.dart';
 
 class AddTransactionForm extends StatefulWidget {
@@ -18,62 +19,74 @@ class _AddTransactionFormState extends State<AddTransactionForm> {
   final TextEditingController _amountController = new TextEditingController();
   final TextEditingController _detailController = new TextEditingController();
 
+  bool loading = false;
+
   @override
   Widget build(BuildContext context) {
     return Container(
       padding: EdgeInsets.symmetric(horizontal: 30),
       child: Column(
         children: [
-          SizedBox(
-            height: 10,
-          ),
+          SizedBox(height: 10),
           Subtitle(
             text: "New Transaction",
           ),
-          SizedBox(
-            height: 10,
-          ),
+          SizedBox(height: 10),
           AppTextField(
             labelText: "Amount",
             keyboardType: TextInputType.number,
             controller: _amountController,
           ),
-          SizedBox(
-            height: 10,
-          ),
+          SizedBox(height: 10),
           AppTextField(
             labelText: "Description",
             controller: _detailController,
           ),
-          SizedBox(
-            height: 10,
-          ),
+          SizedBox(height: 10),
           MaterialButton(
             height: 50,
             minWidth: MediaQuery.of(context).size.width - 60,
             color: Color(0xFF5BC8AA),
-            onPressed: () async {
-              await _createTransaction();
-              Navigator.pop(context);
-            },
-            child: Text(
-              "Confirm",
-              style: TextStyle(color: Colors.white),
-            ),
-          )
+            disabledColor: Colors.grey[400],
+            onPressed: !loading ? _onConfirmPressed : null,
+            child: !loading
+                ? Text(
+                    "Confirm",
+                    style: TextStyle(color: Colors.white),
+                  )
+                : ButtonProgress(),
+          ),
         ],
       ),
     );
+  }
+
+  _onConfirmPressed() async {
+    _setLoading(true);
+    try {
+      await _createTransaction();
+      Navigator.pop(context);
+    } catch (err) {
+      _setLoading(false);
+    }
   }
 
   _createTransaction() async {
     var amount = double.parse(this._amountController.text);
     var detail = this._detailController.text;
     var dateTime = DateTime.now();
+
     var transaction =
         new Transaction(amount: amount, dateTime: dateTime, detail: detail);
     await _transactionsService.create(transaction: transaction);
+
     Provider.of<TransactionModel>(context, listen: false).add(transaction);
+  }
+
+  _setLoading(bool loading) {
+    setState(() {
+      this.loading = loading;
+    });
   }
 
   @override
