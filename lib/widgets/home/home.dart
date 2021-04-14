a9c7d6105030afb408460ca6348baed7d7297cb2
mcarlomagno <marcoscarlomagno1@gmail.com>
2021-04-13 21:32:07 -0300

    fix: bottom sheet with keyboard

--- lib/widgets/home/home.dart
+++ lib/widgets/home/home.dart
@@ -51,6 +51,10 @@ class _MyHomePageState extends State<MyHomePage> {
           final paymentModel =
               Provider.of<PaymentModel>(context, listen: false);
           showModalBottomSheet(
+              shape: RoundedRectangleBorder(
+                  borderRadius:
+                      BorderRadius.vertical(top: Radius.circular(25.0))),
+              isScrollControlled: true,
               context: context,
               builder: (BuildContext context) {
                 return ListenableProvider.value(
